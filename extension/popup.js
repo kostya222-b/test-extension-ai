51e256e543ef3e990cc077d257f4a498aeb69319
kostya222-b <kostyabryazgun@gmail.com>
2026-03-11 10:06:15 +0300

    1

--- extension/popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('toggleModal').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length > 0) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "toggleModal" });
-      }
-    });
-  });
-});
